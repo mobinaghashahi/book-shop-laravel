b859150449b871b2d5e3c752b6074b11dbcce7b2
Mobin Aghashahi <mobinaghashahi13@gmail.com>
2022-12-09 22:21:36 +0330

    change unit price book

--- resources/views/shop/products/item.blade.php
+++ resources/views/shop/products/item.blade.php
@@ -13,8 +13,8 @@
                     @php
                         $wishlist = App\Models\Wishlist::where('id_book',$book->id)->where('id_user',Auth::user()->id)->get()
                     @endphp
-                    
-                    <a href="#" onclick="event.preventDefault(); 
+
+                    <a href="#" onclick="event.preventDefault();
                         document.getElementById('remove_from_wishlist_{{$book->id}}').submit();">
                         <span class="icon_heart"></span>
                     </a>
@@ -25,7 +25,7 @@
                 </li>
             @else
                 <li>
-                    <a href="#" onclick="event.preventDefault(); 
+                    <a href="#" onclick="event.preventDefault();
                         document.getElementById('add_to_wishlist_{{$book->id}}').submit();">
                         <span class="icon_heart_alt"></span>
                     </a>
@@ -36,7 +36,7 @@
                 </li>
             @endif
             <li><a href="#" onclick="event.preventDefault();document.getElementById('add_to_card_{{$book->id}}').submit();"><span class="icon_cart_alt"></span></a>
-        
+
                 <form id="add_to_card_{{$book->id}}" action="{{URL::to('/add-to-carts')}}" method="POST" style="display: none;">
                     {{ csrf_field() }}
                     <input type="text" name="id" value="{{$book->id}}">
@@ -46,6 +46,6 @@
     </div>
     <div class="product__item__text">
         <h6><a href="{{asset('/product/detail/'.$book->id)}}">{{ $book->name }}</a></h6>
-        <div class="product__price">$ {{ $book->price }}</div>
+        <div class="product__price">تومان {{ $book->price }}</div>
     </div>
 </div>
