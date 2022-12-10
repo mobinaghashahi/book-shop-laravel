0971974228adb401777f78bb044f8c983041bac0
unknown <mobinaghashahi13@gmail.com>
2022-12-10 15:18:56 +0330

    chage WhishList language.

--- resources/views/shop/user/wishlist.blade.php
+++ resources/views/shop/user/wishlist.blade.php
@@ -7,8 +7,8 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="breadcrumb__links">
-                    <a href="{{URL::TO('/home')}}"><i class="fa fa-home"></i> Home</a>
-                    <span>Wishlist</span>
+                    <a href="{{URL::TO('/home')}}"><i class="fa fa-home"></i> خانه</a>
+                    <span>علاقمندی ها</span>
                 </div>
             </div>
         </div>
@@ -23,7 +23,7 @@
             <div class="col-lg-12 col-md-12">
                 <div class="row">
                     @if($books_wishlist->count()==0)
-                        <div class="col-lg-12 text-center">Chưa thích sản phẩm nào</div>
+                        <div class="col-lg-12 text-center">هنوز محصول مورد علاقه ای وجود ندارد!</div>
                     @else
                         @foreach($books_wishlist as $key=>$data)
                             <div class="col-lg-3 col-md-6">
@@ -33,7 +33,7 @@
                                             <li><a href="{{URL::TO('/product/detail/'.$data->book->id )}}"><span class="arrow_expand"></span></a>
                                             </li>
                                             <li>
-                                                <a href="#" onclick="event.preventDefault(); 
+                                                <a href="#" onclick="event.preventDefault();
                                                     document.getElementById('remove_from_wishlist_{{$data->id}}').submit();">
                                                     <span class="icon_heart"></span>
                                                 </a>
@@ -44,7 +44,7 @@
                                             </li>
                                             <li>
                                                 <a href="#" onclick="event.preventDefault();document.getElementById('add_to_card_{{$data->book->id}}').submit();"><span class="icon_cart_alt"></span></a>
-            
+
                                                 <form id="add_to_card_{{$data->book->id}}" action="{{URL::TO('/add-to-carts')}}" method="POST" style="display: none;">
                                                     {{ csrf_field() }}
                                                     <input type="text" name="id" value="{{$data->book->id}}">
